e18210d32f0fa9637ff9cca1afae98217a1b9ee0
Hmef <hmeftaah@gmail.com>
2023-06-19 20:03:14 +0200

    MainClass - PrivateConstructor Test

--- src/com/PrivateConstrucor/MainClass.java
+++ src/com/PrivateConstrucor/MainClass.java
@@ -4,6 +4,6 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		PrivateConstructor privateConstructor = new PrivateConstructor();
+//		PrivateConstructor privateConstructor = new PrivateConstructor();
 	}
 }
